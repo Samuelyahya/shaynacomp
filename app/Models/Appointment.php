395550529abfcf3d3fbd1208bab442289eeb04cd
Samuel Yahya <samuelyahya25@gmail.com>
2025-09-03 22:34:37 +0700

    Make model, migration, controller

---
+++ app/Models/Appointment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Appointment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone_number',
+        'meeting_at',
+        'email',
+        'budget',
+        'brief',
+        'product_id',
+    ];
+
+    protected $casts = [
+        'meeting_at' => 'date',
+    ];
+
+    public function product():BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
---
+++ app/Models/CompanyAbout.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CompanyAbout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'type',
+    ];
+
+    public function keypoints():HasMany
+    {
+        return $this->hasMany(CompanyKeypoint::class);
+    }
+}
